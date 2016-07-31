9fad26a37d8656668d19279362d103b2d118697f
Rafael <rafaelk-f@hotmail.com>
2016-07-31 11:53:25 -0300

    .

---
+++ app/pages/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'build/pages/login/login.html',
+})
+
+
+export class Login {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
---
+++ app/pages/usuario-cadastro/usuario-cadastro.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'build/pages/usuario-cadastro/usuario-cadastro.html',
+})
+
+
+export class UsuarioCadastro
+{
+  constructor(private nav: NavController) {
+
+  }
+
+}
